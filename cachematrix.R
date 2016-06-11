52f567389eead9af00d43750028b19a6f845e7d0
Baris Kalayci <kaleandroid@gmail.com>
2016-06-11 22:36:14 +0200

    Comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+#These functions are helping to create a matrix-object and cache the inverse
+#of the matrix since the computation could be take too long time.
 
-## Write a short comment describing this function
-#This function creates a matrix-object to be able to cache the inverse of that matrix
+#This function creates a matrix-object to be able to cache the inverse of a matrix.
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -12,17 +11,15 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setmat <- function(solve) m <<- solve
   getmat <- function() m
-  list(set = set, get = get, setmat = setmat, getmat = getmat)
+  list(set = set, get = get, setmat = setmat, getmat = getmat) #return the "special" matrix
 }
 
-
-## Write a short comment describing this function
 #This function chaches an inverted matrix.
 #If it is needed, it will be returned without recalculating it.
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   m <- x$getmat()
-  if(!is.null(m)) {
+  if(!is.null(m)) { #if the matrix is not null
     message("getting cached data")
     return(m)
   }
